d1d51b2e325d81f5dac7ec9069b7f6546532b799
NaamaCahen <naama5263@gmail.com>
2023-05-17 11:08:22 +0300

    relationships updates

--- travel_app/models.py
+++ travel_app/models.py
@@ -19,41 +19,41 @@ class Hike(db.Model):
     hike_name = db.Column(db.String(100), nullable=False)
     length_km = db.Column(db.Float, nullable=False)
     time = db.Column(db.Float, nullable=False)
-    region = db.Column(db.Integer, db.ForeignKey('region.region_id'))
-    level = db.Column(db.Integer, db.ForeignKey('level.level_id'))
-    season = db.Column(db.Integer, db.ForeignKey('season.season_id'))
-    category = db.Column(db.Integer, db.ForeignKey('category.category_id'))
+    region = db.Column(db.Integer, db.ForeignKey('regions.region_id'))
+    level = db.Column(db.Integer, db.ForeignKey('levels.level_id'))
+    season = db.Column(db.Integer, db.ForeignKey('seasons.season_id'))
+    category = db.Column(db.Integer, db.ForeignKey('categories.category_id'))
     for_who = db.Column(db.Integer, db.ForeignKey('people.people_id'))
     water = db.Column(db.Boolean)
-    pictures = db.relationship('Picture', backref='hike', lazy='dynamic')
+    pictures = db.relationship('Picture', backref='hikes', lazy='dynamic')
 
 
 class Region(db.Model):
     __tablename__ = 'regions'
     region_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     region_name = db.Column(db.String(32), nullable=False)
-    hikes = db.relationship('Hike', backref='region', lazy='dynamic')
+    hikes = db.relationship('Hike', backref='regions', lazy='dynamic')
 
 
 class Level(db.Model):
     __tablename__ = 'levels'
     level_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     level_name = db.Column(db.String(32), nullable=False)
-    hikes = db.relationship('Hike', backref='level', lazy='dynamic')
+    hikes = db.relationship('Hike', backref='levels', lazy='dynamic')
 
 
 class Season(db.Model):
     __tablename__ = 'seasons'
     season_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     season_name = db.Column(db.String(32), nullable=False)
-    hikes = db.relationship('Hike', backref='season', lazy='dynamic')
+    hikes = db.relationship('Hike', backref='seasons', lazy='dynamic')
 
 
 class Category(db.Model):
     __tablename__ = 'categories'
     category_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     category_name = db.Column(db.String(32), nullable=False)
-    hikes = db.relationship('Hike', backref='category', lazy='dynamic')
+    hikes = db.relationship('Hike', backref='categories', lazy='dynamic')
 
 
 class ForWho(db.Model):
@@ -67,4 +67,4 @@ class Picture(db.Model):
     __tablename__ = 'pictures'
     picture_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     url = db.Column(db.String(128), nullable=False)
-    hike_id = db.Column(db.Integer, db.ForeignKey('hike.hike_id'))
+    hike_id = db.Column(db.Integer, db.ForeignKey('hikes.hike_id'))
